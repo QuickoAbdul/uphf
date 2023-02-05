4ab2b5825fec6ea1311f9de43c98df39b6a961bf
abdul <quickoabdul@gmail.com>
2023-02-05 10:41:09 +0100

    First

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.css']
+})
+export class AccueilComponent {
+  president = 'Artiba';
+  inst1 = 'ISH';
+  inst2 = 'IUT';
+  inst3 = 'INSA';
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'uphf';
+  title = 'UPHF';
+  annee = '2022-2023';
 }
